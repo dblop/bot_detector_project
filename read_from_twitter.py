cd022b67de6883e1f637ce1f6427f15ee1bbb8cc
Diego <diego.lira91@gmail.com>
2020-04-12 00:40:42 -0300

    fix read twitter

--- read_from_twitter.py
+++ read_from_twitter.py
@@ -16,6 +16,11 @@ auth = tweepy.AppAuthHandler(consumer_key, consumer_secret)
 
 api = tweepy.API(auth)
 
+
+
+param_list = ["dataset","label","profile_banner_url","id","id_str","name","screen_name","location","profile_location","description","url","protected","followers_count","friends_count","listed_count","favourites_count","utc_offset","time_zone","geo_enabled","verified","statuses_count","lang","contributors_enabled","is_translator","is_translation_enabled","profile_background_color","profile_background_image_url","profile_background_image_url_https","profile_background_tile","profile_image_url","profile_image_url_https","profile_link_color","profile_sidebar_border_color","profile_sidebar_fill_color","profile_text_color","profile_use_background_image","has_extended_profile","default_profile","default_profile_image","following","follow_request_sent","notifications","translator_type","created_at"]
+
+
 count = 0 
 with open("data.txt") as f:
     for line in f:
@@ -28,20 +33,24 @@ with open("data.txt") as f:
         label = data[2].strip('\n')
         
         dict_data = dict()
+        #Initialize everything as null. API isn't consistent about null values and might not return the param otherwise.
+        for param in param_list:
+            dict_data[param] = None
+
         dict_data["dataset"] = dataset
         dict_data["label"] = label
 
         try:
             user = api.get_user(user_id=user_id)
         except RateLimitError:
-            dict_data["user_id"] = user_id
+            dict_data["id_str"] = user_id
             time.sleep(60)
             df_a = pd.DataFrame(dict_data, index=[0])
             df_a.to_csv("retry_later_users.csv",mode="a",index=False,header=False)
             print(f"Iteration {count} hit a RateLimitError")
             continue            
         except TweepError as e:
-            dict_data["user_id"] = user_id
+            dict_data["id_str"] = user_id
             # error codes and messages:
             # https://developer.twitter.com/en/docs/basics/response-codes
             # if there's any non-Twitter error we don't get the message!
@@ -53,18 +62,11 @@ with open("data.txt") as f:
             df_a.to_csv("suspended_users.csv",mode="a",index=False,header=False)
             continue
         
-        #
         params = list(user.__dict__.keys())
-        params.remove("_api")
-        params.remove("_json")
-        params.remove("status")
-        params.remove("entities")
-        params.remove("created_at")
-        
-        #API doesnt return the null profile_banner_url if there's none
-        dict_data["profile_banner_url"] = None
+
         for param in params:
-            dict_data[param] = getattr(user,param)
+            if param in param_list:
+                dict_data[param] = getattr(user,param)
 
         df_a = pd.DataFrame(dict_data, index=[0])
 
